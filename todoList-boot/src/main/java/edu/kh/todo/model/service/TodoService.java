9fdc9c2588a0ad767daa8926d5ce21464cde9963
nomyeongjin <bbagifa2555@gmail.com>
2024-03-25 17:22:09 +0900

    240325_TodoList 기능 추가 + 비동기식 요청 (ajax)

--- todoList-boot/src/main/java/edu/kh/todo/model/service/TodoService.java
+++ todoList-boot/src/main/java/edu/kh/todo/model/service/TodoService.java
@@ -2,10 +2,12 @@ package edu.kh.todo.model.service;
 
 import java.util.Map;
 
+import edu.kh.todo.model.dto.Todo;
+
 public interface TodoService {
 
-	/** 할 일 목록 + 완료된 할일 개수 조회
-	 * @return
+	/** 할 일 목록 + 완료된 할 일 개수 조회
+	 * @return map
 	 */
 	Map<String, Object> selectAll();
 
@@ -15,5 +17,39 @@ public interface TodoService {
 	 * @return result
 	 */
 	int addTodo(String todoTitle, String todoContent);
+
+	/** 할 일 상세 조회
+	 * @param todoNo
+	 * @return todo
+	 */
+	Todo todoDetail(int todoNo);
+
+	/** 할 일 삭제
+	 * @param todoNo
+	 * @return result
+	 */
+	int todoDelete(int todoNo);
+
+	/** 할 일 수정
+	 * @param todo
+	 * @return result
+	 */
+	int todoUpdate(Todo todo);
+
+	/** 완료 여부 변경
+	 * @param todo
+	 * @return result
+	 */
+	int changeComplete(Todo todo);
+
+	/** 전체 할일 개수 조회
+	 * @return totalcount
+	 */ 
+	int getTotalCount();
+
+	/** 완료된 할 일 개수 조회
+	 * @return completeCount
+	 */
+	int getCompleteCount();
 
 }
--- todoList-boot/src/main/java/edu/kh/todo/model/service/TodoServiceImpl.java
+++ todoList-boot/src/main/java/edu/kh/todo/model/service/TodoServiceImpl.java
@@ -25,47 +25,99 @@ import edu.kh.todo.model.mapper.TodoMapper;
 
 // 모든 종류의 예외 발생 시 rollback 수행
 @Transactional(rollbackFor = Exception.class)
-@Service // 비지니스 로직(데이터 가공, 트랜잭션 처리) 역할 명시
+@Service // 비즈니스 로직(데이터 가공, 트랜잭션 처리) 역할 명시
 		 // + Bean 등록
 public class TodoServiceImpl implements TodoService{
-
+	
 	@Autowired // DI
 	private TodoMapper mapper;
 	
-	/////// 할일 목록 + 완료된 할일 개수 --------------------------------
+	
+	// 할 일 목록 + 완료된 할 일 개수 조회
 	@Override
 	public Map<String, Object> selectAll() {
-
+		
 		// 1. 할 일 목록 조회
-		List<Todo> todoList = mapper.selectAll(); 
+		List<Todo> todoList = mapper.selectAll();
 		
 		// 2. 완료된 할 일 개수 조회
 		int completeCount = mapper.getCompleteCount();
 		
+		
 		// Map으로 묶어서 반환
 		Map<String, Object> map = new HashMap<>();
 		
-		map.put("todoList",todoList);
-		map.put("completeCount",completeCount);
+		map.put("todoList", todoList); 
+		map.put("completeCount", completeCount); 
 		
 		return map;
 	}
-
-	////////// 할 일 추가 --------------------------------------------------
+	
+	
+	// 할 일 추가
 	@Override
 	public int addTodo(String todoTitle, String todoContent) {
 		
-		// Connection 생성 반환 X
-		// 트랜잭션 제어처리 -> @Transactional 어노테이션
+		// Connection 생성/반환 X
+		// 트랜잭션 제어 처리 -> @Transactional 어노테이션
 		
-		// Mybatis에서 SQL에 전달할 수 있는 파라미터의 개수는
-		// 오직 1개
-		// -> todoTitle, todoContent를 TodoDTO 로 묶어서 전달
+		// 마이바티스에서 SQL에 전달할 수 있는 파라미터의 개수는
+		// 오직 1개!!!
+		// -> todoTitle, todoContent를 Todo DTO로 묶어서 전달
+		
 		Todo todo = new Todo();
 		todo.setTodoTitle(todoTitle);
 		todo.setTodoContent(todoContent);
 		
 		return mapper.addTodo(todo);
+	}
+	
+	
+	
+	// 할 일 상세 조회
+	@Override
+	public Todo todoDetail(int todoNo) {
+		return mapper.todoDetail(todoNo);
+	}
+	
+	
+	
+	// 할 일 삭제
+	@Override
+	public int todoDelete(int todoNo) {
+		return mapper.todoDelete(todoNo);
+	}
+	
+	
+	
+	// 할 일 수정
+	@Override
+	public int todoUpdate(Todo todo) {
+		
+		// 마이바티스 객체를 이용할 때
+		// SQL에 전달할 수 있는 파라미터는 오직 1개!!!
+		// -> 여러 데이터를 전달하고 싶으면 Map, DTO, List로 묶어서 전달
+		return mapper.todoUpdate(todo);
+	}
+	
+	
+	// 완료 여부 변경
+	@Override
+	public int changeComplete(Todo todo) {
+		return mapper.changeComplete(todo);
+	}
+
+
+	// 전체 할일 개수 조회
+	@Override
+	public int getTotalCount() {
+		return mapper.getTotalCount();
+	}
+	
+	// 완료된 할일 개수 조회
+	@Override
+	public int getCompleteCount() {
+		return mapper.getCompleteCount();
 	}
 	
 }
